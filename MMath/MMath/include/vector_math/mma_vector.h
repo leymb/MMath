8d7f726e51a23a79560b399462989793fe262f13
LeyMB <leymb@pm.me>
2022-01-21 17:29:26 +0100

    [CHANGE] Removed w coordinate
    - mma_vector.h/.cpp: removed w coordinate

--- MMath/MMath/include/vector_math/mma_vector.h
+++ MMath/MMath/include/vector_math/mma_vector.h
@@ -7,13 +7,11 @@ public:
 	Vec3D();
 	Vec3D(float a_Value);
 	Vec3D(float x, float y, float z);
-	Vec3D(float x, float y, float z, float w);
 
 
 	float m_X;
 	float m_Y;
 	float m_Z;
-	float m_W;
 
 	Vec3D operator + (const Vec3D& a_Vector) const;
 	Vec3D operator - (const Vec3D& a_Vector) const;
