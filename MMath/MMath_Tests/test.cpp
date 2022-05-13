c34fc3b40e9d70a738ade5d6a4e779aee4cd615c
Mel <melvin.rother@pm.me>
2022-05-13 16:17:39 +0200

    [Added] Reintroduced Unit Tests
    - added back unit test project for automated testing

---
+++ MMath/MMath_Tests/test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include <vector_math/mma_vector.h>
+
+TEST(TestCaseName, TestName) {
+
+	Vec3D test = {};
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
---
+++ MMath/MMath_Tests/tests/vec3D_tests.cpp
@@ -0,0 +1,76 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include <vector_math/mma_vector.h>
+#include <matrix_math/mma_mat3.h>
+
+class Vec3DTestF : public ::testing::Test
+{
+protected:
+	Vec3DTestF()
+	{
+	}
+
+	void SetUp() override
+	{
+		Test::SetUp();
+	}
+
+	void TearDown() override
+	{
+		Test::TearDown();
+	}
+
+	Vec3D m_TestVec3D_ = {};
+
+	float m_Divisor_ = 2.0f;
+};
+
+TEST_F(Vec3DTestF, Constructor_Empty)
+{
+	m_TestVec3D_ = {};
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_X, 0.0f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Y, 0.0f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Z, 0.0f);
+}
+
+TEST_F(Vec3DTestF, Constructor_OneFloat)
+{
+	m_TestVec3D_ = { 4.5f };
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_X, 4.5f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Y, 4.5f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Z, 4.5f);
+}
+
+TEST_F(Vec3DTestF, Constructor_ThreeFloats)
+{
+	m_TestVec3D_ = { 3.6f, 14.2f, 4.5f };
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_X, 3.6f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Y, 14.2f);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Z, 4.5f);
+}
+
+TEST_F(Vec3DTestF, Operators_Division)
+{
+	// division 
+	m_TestVec3D_ = { 1.5f, 2.f, 1.f };
+
+	Vec3D t_Quotient = m_TestVec3D_ / m_Divisor_;
+
+	EXPECT_FLOAT_EQ(t_Quotient.m_X, m_TestVec3D_.m_X / m_Divisor_);
+	EXPECT_FLOAT_EQ(t_Quotient.m_Y, m_TestVec3D_.m_Y / m_Divisor_);
+	EXPECT_FLOAT_EQ(t_Quotient.m_Z, m_TestVec3D_.m_Z / m_Divisor_);
+}
+
+TEST_F(Vec3DTestF, Operators_DivisionAssignment)
+{
+	// division 
+	m_TestVec3D_ = { 1.5f, 2.f, 1.f };
+	Vec3D t_TempVec = m_TestVec3D_;
+
+	// call to MUT
+	m_TestVec3D_ /= m_Divisor_;
+
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_X, t_TempVec.m_X / m_Divisor_);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Y, t_TempVec.m_Y / m_Divisor_);
+	EXPECT_FLOAT_EQ(m_TestVec3D_.m_Z, t_TempVec.m_Z / m_Divisor_);
+}
